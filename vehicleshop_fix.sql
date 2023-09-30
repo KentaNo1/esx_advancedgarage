2d28c7d272d29c1e50e093e83e7759ca6a015387
Kenta <102387086+KentaKK@users.noreply.github.com>
2023-10-01 00:45:54 +0200

    Delete vehicleshop_fix.sql

--- vehicleshop_fix.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
-  ('blue_phone', 'Mavi Telefon', 0, 0, 1),
-  ('green_phone', 'Yeşil Telefon', 0, 0, 1),
-  ('white_phone', 'Beyaz Telefon', 0, 0, 1);
