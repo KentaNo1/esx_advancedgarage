405e17e9ca855578aaea42d9668a83e7421cd585
Kenta <102387086+KentaKK@users.noreply.github.com>
2023-10-01 00:36:59 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -194,31 +194,21 @@ Config.AmbulancePounds = {
 
 -- Start of Cars
 Config.CarPounds = {
-
 	Pound_LosSantos = {
-
 		PoundPoint = { x = 408.61, y = -1625.47, z = 28.29 },
 
 		SpawnPoint = { x = 405.64, y = -1643.4, z = 27.61, h = 229.54 }
-
 	},
 
 	Pound_Sandy = {
-
 		PoundPoint = { x = 1651.38, y = 3804.84, z = 37.65 },
-
 		SpawnPoint = { x = 1627.84, y = 3788.45, z = 33.77, h = 308.53 }
-
 	},
 
 	Pound_Paleto = {
-
 		PoundPoint = { x = -234.82, y = 6198.65, z = 30.94 },
-
 		SpawnPoint = { x = -230.08, y = 6190.24, z = 30.49, h = 140.24 }
-
 	}
-
 }
 -- End of Cars
 
